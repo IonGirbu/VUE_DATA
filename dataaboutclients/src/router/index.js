14382ef2272f27580a674dc5dea88a6f73e4a1a7
IonGirbu <iongirbu12@mail.ru>
2022-04-06 13:05:02 +0300

    update

---
+++ dataaboutclients/src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/Alexandr',
+      name: 'Alexandr',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Alexandr.vue')
+    },
+    {
+      path: '/John',
+      name: 'John',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/John.vue')
+    },
+    {
+      path: '/Merlin',
+      name: 'Merlin',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Merlin.vue')
+    }
+  ]
+})
+
+export default router
---
+++ dataaboutclients/src/views/John.vue
@@ -0,0 +1,19 @@
+<template>
+  <div >
+    <h3>Name: John</h3>
+    <h3>Surname: Mercury</h3>
+    <h3>Age: 19</h3>
+    <h3>About: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, corporis sint deleniti veritatis odio assumenda accusantium mollitia, hic dolore exercitationem voluptatibus minus dolores maxime in. Hic blanditiis saepe ipsa fugiat.</h3>
+    <h3>Email: sJsob@mail.ru</h3>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
